f908c4942c34a2df4f9a3ec13be8b0e511d97c6b
Jerry Soto <healthCareMathModeler@gmail.com>
2013-01-29 11:34:14 -0800

    1STphysioNetCommit

---
+++ physioNet/physioNet.py
@@ -0,0 +1,23 @@
+'''
+Created on Jan 29, 2013
+
+@author: JerrySoto
+'''
+import csv
+import numpy as np
+import math
+#from separate_column import *
+
+import sys
+csv.field_size_limit(sys.maxint)
+
+inputPath='C:\Users\JerrySoto\Desktop\VPS\physioNet_MIT_challenge2012\set-a'
+
+fileName='132539.txt'
+
+inFileName=inputPath+'\\'+fileName
+
+InFile = open(inFileName,'rb')
+csvRead = csv.reader(InFile,delimiter=',')
+
+temp=csvRead.next()
